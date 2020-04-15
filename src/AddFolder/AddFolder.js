fdcc5f73bb6d102d9ae000d9f2b29553e24380c0
Garrett James <garrettjames55@gmail.com>
2020-04-14 23:12:10 -0400

    initial commit

---
+++ src/AddFolder/AddFolder.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import NotefulForm from "../NotefulForm/NotefulForm";
+import ApiContext from "../ApiContext";
+import config from "../config";
+
+class AddFolder extends Component {
+  static contextType = ApiContext;
+
+  handleSubmit = e => {
+    const folder = {
+      name: e.target["folderName"].value
+    };
+
+    fetch(`${config.API_ENDPOINT}/folder`, {
+      method: "POST",
+      headers: { "content-type": "application/JSON" },
+      body: JSON.stringify(folder)
+    })
+      .then(response => {
+        if (!response.ok) {
+          return response.json().then(e => Promise.reject(e));
+        }
+        return response.json();
+      })
+      .then(folder => {
+        this.props.history.push("/");
+        this.context.AddFolder(folder);
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  };
+
+  validateName = event => {
+    event.preventDefault();
+
+    const name = event.target["folderName"].value.trim();
+    if (name.length === 0) {
+      return this.context.setError(true);
+    } else if (name.length < 1) {
+      return this.context.setError(true);
+    } else {
+      this.handleSubmit(event);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Create A Folder</h2>
+        <NotefulForm onSubmit={this.validateName}>
+          {this.context.error ? "**Invalid Name **" : ""}
+
+          <div>
+            <label htmlFor="folderName">Folder Name</label>
+            <input type="text" id="folderName"></input>
+
+            <button type="submit">Add Folder</button>
+          </div>
+        </NotefulForm>
+      </div>
+    );
+  }
+}
+
+export default AddFolder;
---
+++ src/ErrorBoundary/ErrorPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class ErrorPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      error: false
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      error: true
+    };
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div className="ErrorPage">
+          <h1>Something went wrong!</h1>
+          <p>Refresh the page</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+ErrorPage.propTypes = {
+  children: PropTypes.object.isRequired
+};
